548b88f94e951d8dd2d080742524be2d0ccde7fc
ranjezthanjez <ranjezthanjez@gmail.com>
2022-05-19 12:38:06 +0300

    strings in python

---
+++ Untitled-1.py
@@ -0,0 +1,4 @@
+f_num = input("enter the first number")
+s_num = input("enter the second number")
+s_of_nums =int (f_num) +int (s_num)
+print("the sum of the 2 number is"str(s_num))
---
+++ strings.py
@@ -0,0 +1,21 @@
+""""name="moses karanja"i am from Thika"""
+city="nairobi"
+# .upper() to convert to upper case
+print (city.upper ())
+uni="JKUAT"
+print (uni.lower())
+fruit="pineaple"
+print(fruit[:2])
+print (fruit[5:])
+print(fruit[5:])
+print(fruit[-2:])
+print (fruit[-5])
+#strip() remove space before the text
+ f_name "moses karanja"
+ print(f_name.strip())
+ f-name="moses"
+ s-name="karanja"
+ full_name=f_name+s_name
+ print(full_name)
+ city="mombasa"
+ print(len(city))
